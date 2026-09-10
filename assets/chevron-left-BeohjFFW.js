f0222ce3658d4b84bcfa0e367f5c16d02002410a
launchers222 <325641259+launchers222@users.noreply.github.com>
2026-09-10 14:50:46 +0200

    Publish site

--- assets/chevron-left-BeohjFFW.js
+++
@@ -1,8 +0,0 @@
-import{w as o}from"./core-DGBd2lfG.js";import"./index-75lbGyQ7.js";import"./events-DS5ycqBl.js";import"./index.es-7kc7BBs5.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M11.04 1.46a1 1 0 0 1 0 1.41L5.91 8l5.13 5.13a1 1 0 1 1-1.41 1.41L3.79 8.71a1 1 0 0 1 0-1.42l5.84-5.83a1 1 0 0 1 1.41 0Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{p as chevronLeftSvg};
--- assets/cursor-Cql9vu_a.js
+++
@@ -1,3 +0,0 @@
-import{w as o}from"./core-DGBd2lfG.js";import"./index-75lbGyQ7.js";import"./events-DS5ycqBl.js";import"./index.es-7kc7BBs5.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
