a6c773522ac43211c03277badc41db304b0bf650
launchers222 <325641259+launchers222@users.noreply.github.com>
2026-09-10 18:33:30 +0200

    Publish site

--- assets/cursor-DHgQ-PrW.js
+++
@@ -1,3 +0,0 @@
-import{w as o}from"./core-esShzrkd.js";import"./index-55XpJmRG.js";import"./events-BN3QjNeG.js";import"./index.es-CB3mHuMT.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
