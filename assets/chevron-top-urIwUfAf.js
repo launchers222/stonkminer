e9ef0ebf67332c1653ceb355da69d9887f072de9
launchers222 <325641259+launchers222@users.noreply.github.com>
2026-09-10 18:00:58 +0200

    Publish site

--- assets/chevron-top-urIwUfAf.js
+++
@@ -1,8 +0,0 @@
-import{w as o}from"./core-Dws3HVUr.js";import"./index-DOI17cit.js";import"./events-COVzDpVL.js";import"./index.es-uLED-cbJ.js";import"./index-nibyPLVP.js";const t=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M14.54 11.04a1 1 0 0 1-1.41 0L8 5.92l-5.13 5.12a1 1 0 1 1-1.41-1.41l5.83-5.84a1 1 0 0 1 1.42 0l5.83 5.84a1 1 0 0 1 0 1.41Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{t as chevronTopSvg};
